2b25ca66b6b17f8ddc32dcd79ac65f26a80a4338
anuhya2 <reddyanuhya3@gmail.com>
2023-04-27 11:08:41 -0600

    Anuhya: Updated server code with printfs

--- server/server.c
+++ server/server.c
@@ -14,8 +14,12 @@
 #include <netdb.h>
 
 
-
 int signal_interrupt = 0;
+int server_socket_fd;
+int new_fd;
+
+struct sockaddr_in their_addr;
+struct addrinfo hints, *server_info;
 
 
 
@@ -31,7 +35,7 @@ void signal_handler(int signum)
         syslog(LOG_DEBUG, "Caught signal SIGTERM, exiting!!");
         signal_interrupt = 1;
     }
-    
+    //close fds
     exit(0);
 }
 
@@ -68,7 +72,8 @@ int daemon_creation()
 
 int main(int argc, char *argv[])
 {
-   // char ip_string[INET_ADDRSTRLEN];
+    int ret; 
+    const int yes = 1;
     
     openlog(NULL, 0, LOG_USER);
     signal(SIGINT, signal_handler);
@@ -91,56 +96,51 @@ int main(int argc, char *argv[])
 	return -1;
     }
  
-    struct sockaddr their_addr;
-    socklen_t addr_size = sizeof(their_addr);
-    struct addrinfo hints, *server_info;
+    server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     
-    int server_socket_fd;
-    int new_fd;// bytes_sent;   ///CHANGE THESE
-
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET;  
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; 
-    hints.ai_protocol = 0;    
-
-
-    int ret;
-
-    ret = getaddrinfo(NULL, "9000", &hints, &server_info);
-    if( ret !=0 )
-    {
-        syslog(LOG_ERR, "Error: getaddrinfo() with code, %d", errno);
-       // return(1);
-    }
-
-    server_socket_fd = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
     if( server_socket_fd == -1 )
     {
         syslog(LOG_ERR, "Error: socket() with code, %d", errno);
-      //  return(1);
+        exit(1);
+    }
+    printf("created socket\n");
+   
+    hints.ai_flags = AI_PASSIVE;
+
+    ret = getaddrinfo(NULL, "9000", &hints, &server_info);
+    printf("getaddrinfo\n");
+    
+    if(ret != 0)
+    {
+        syslog(LOG_ERR, "Error: getaddrinfo() with code, %d", errno);
+        exit(1);
     }
 
-    const int yes = 1;
-    if (setsockopt(server_socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) < 0)
+    
+    ret = setsockopt(server_socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes))
+    
+    if (ret != 0)
     {
     	syslog(LOG_ERR, "Error: setsocketopt() with code, %d", errno);
-    //  return(1);
+        exit(1);
     }
+    printf("setsockopt\n"); 
     
-    int flags = fcntl(server_socket_fd, F_GETFL);
-    fcntl(server_socket_fd, F_SETFL, flags | O_NONBLOCK);
+    //int flags = fcntl(server_socket_fd, F_GETFL);
+    //fcntl(server_socket_fd, F_SETFL, flags | O_NONBLOCK);
     
-    ret = bind(server_socket_fd, server_info->ai_addr, server_info->ai_addrlen);
-    if( ret != 0 )
+    ret = bind(server_socket_fd, server_info->ai_addr, sizeof(struct sockaddr));
+
+    if(ret != 0)
     {
         syslog(LOG_ERR, "Error: bind() with code, %d", errno);
-        //return(1);
+        exit(1);
     }
+    printf("bind\n");
 
     freeaddrinfo(server_info);
 
-    if( is_daemon )
+    if(is_daemon)
     {
         if(daemon_creation() != 0)
         {
@@ -149,29 +149,31 @@ int main(int argc, char *argv[])
     }
 
 
-    ret = listen(server_socket_fd, 10);
-    if( ret != 0 )
+    ret = listen(server_socket_fd, 1);
+    if(ret != 0)
     {
         syslog(LOG_ERR, "Error: listen() with code, %d", errno);
+        exit(1);
     }
+    printf("listen success\n");
 
-    new_fd = accept(server_socket_fd, &their_addr, &addr_size);
-    if(new_fd  == -1)
-        {
-            if(errno == EWOULDBLOCK)
-            {
-          		//return(1);
-            }
+    ret = accept(server_socket_fd, &their_addr, &addr_size);
+    if(ret  == -1)
+    {
             syslog(LOG_ERR, "Error : accept with error no : %d", errno);
-            //return(1);
-        }
+            exit(1);
+    }
+    else
+    {
+    	    syslog(LOG_INFO, "Accepted connection from %s", inet_ntoa(their_addr.sin_addr));
+    	    printf("Accepted connected from %s\n", inet_ntoa(client_addr.sin_Addr));
+    }
     
-   // char send_data[] = "Server is working";
     
     while(!signal_interrupt)
     {
-    //int bytes_sent = 
-    send(new_fd,"\n Server is working" , sizeof("\n Server is working"), 0);
+   	 send(new_fd,"\n Server is working" , sizeof("\n Server is working"), 0);
+   	 printf("Inside while loop\n");
     }
     
     close(server_socket_fd);
